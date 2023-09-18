b183d7c42068d56523e1f04f888b7e463e8dfba4
Gbadamosi Mustapha <babamusty7@gmail.com>
2023-09-18 11:16:56 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,15 @@
 #ifndef main_h
 #define main_h
+
 #include <stdio.h>
 #include <unistd.h>
-void reset_to_98(int *n);
+
 int _putchar(char c);
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
+
 
 
 #endif
