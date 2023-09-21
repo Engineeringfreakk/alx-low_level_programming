df0ccaf842ac5853d0aac17fd98bc7fe4f2f0c26
Gbadamosi Mustapha <babamusty7@gmail.com>
2023-09-21 07:00:38 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string - A function that capitalizes all words of a string
+ * @str: capitalize all words of a string
+ * Return: A pointer to the changed string
+ */
+char *cap_string(char *str)
+{
+	int ring = 0;
+
+
+	while (str[ring])
+	{
+		while (!(str[ring] >= 'a' && str[ring] <= 'z'))
+			ring++;
+		if (str[ring - 1] == ' ' ||
+				str[ring - 1] == '\t' ||
+				str[ring - 1] == '\n' ||
+				str[ring - 1] == ',' ||
+				str[ring - 1] == ';' ||
+				str[ring - 1] == '.' ||
+				str[ring - 1] == '!' ||
+				str[ring - 1] == '?' ||
+				str[ring - 1] == '"' ||
+				str[ring - 1] == '(' ||
+				str[ring - 1] == ')' ||
+				str[ring - 1] == '{' ||
+				str[ring - 1] == '}' ||
+				ring == 0)
+			str[ring] -= 32;
+	}
+	return (str);
+}
